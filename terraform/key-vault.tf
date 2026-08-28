b0b67fd5851f0fc50421e7710098dfb87f35b098
A-benslimane <benslimane201440@gmail.com>
2026-08-28 15:25:33 +0200

    fix: stabilize Terraform CI identity

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -21,7 +21,7 @@ resource "azurerm_key_vault" "main" {
 resource "azurerm_role_assignment" "current_user_key_vault_secrets_officer" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
+  principal_id         = "943125bc-c1d5-46c1-96f3-09ef1781da21"
 }
 
 resource "azurerm_key_vault_secret" "postgresql_admin_password" {
