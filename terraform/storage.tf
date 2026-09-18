9b41c624975ea2d36072f9fb4b5a2b829dc9a190
A-benslimane <benslimane201440@gmail.com>
2026-09-18 11:46:28 +0200

    fix: update Azure identity and storage security

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -5,7 +5,8 @@ resource "azurerm_storage_account" "main" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  min_tls_version = "TLS1_2"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
 
   tags = merge(
     local.common_tags,
